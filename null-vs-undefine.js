36f2878855cba93c898172846627182857b922bf
rakibul15 <rakibul15-733@diu.edu.bd>
2020-08-08 19:08:22 +0600

    null vs undefined clear for interview

---
+++ null-vs-undefine.js
@@ -0,0 +1,24 @@
+let pakhi
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return 
+}
+
+
+function add(num1, num2) {
+    console.log(num1,num2);
+}
+
+const result = add(45);
+console.log(result);
+
+const friend = {name: 'smart girl', phone: '99930'};
+console.log(friend.bf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages= [2, 3, 4];
+console.log(ages[11]);
+
