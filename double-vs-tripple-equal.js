ceb621ec90b2187c8b94a7f5717403458e6063d1
rakibul15 <rakibul15-733@diu.edu.bd>
2020-08-10 14:50:04 +0600

    truty and falsy done

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,23 @@
+double check only value but triple equal check value & type.
+const first = 2;
+const second = '2';
+
+if (first === second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
+
+const first_num = 1;
+const second_num = true;
+
+if (first_num === second_num){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
+
